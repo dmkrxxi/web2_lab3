136a7c04b3de1c3a6f2ba9ee8f6a0fe780f6b327
Diana Kornilova <dmkornilova@yandex.com>
2026-04-02 00:53:21 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,12 +1,44 @@
 <form action="" method="POST">
-  <input name="fio" />
-  <select name="year">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
-    }
-    ?>
-  </select>
-  
-  <input type="submit" value="ok" />
+
+  ФИО:<br>
+  <input name="fio" /><br>
+
+  Телефон:<br>
+  <input name="phone" /><br>
+
+  Email:<br>
+  <input name="email" /><br>
+
+  Дата рождения:<br>
+  <input type="date" name="birthdate" /><br>
+
+  Пол:<br>
+  <label><input type="radio" name="gender" value="male"> Мужской</label>
+  <label><input type="radio" name="gender" value="female"> Женский</label><br>
+
+  Любимые языки программирования:<br>
+  <select name="abilities[]" multiple="multiple">
+    <option value="1">Pascal</option>
+    <option value="2">C</option>
+    <option value="3">C++</option>
+    <option value="4">JavaScript</option>
+    <option value="5">PHP</option>
+    <option value="6">Python</option>
+    <option value="7">Java</option>
+    <option value="8">Haskel</option>
+    <option value="9">Clojure</option>
+    <option value="10">Prolog</option>
+    <option value="11">Scala</option>
+    <option value="12">Go</option>
+  </select><br>
+
+  Биография:<br>
+  <textarea name="bio"></textarea><br>
+
+  <label>
+    <input type="checkbox" name="contract">
+    С контрактом ознакомлен(а)
+  </label><br><br>
+
+  <input type="submit" value="Сохранить">
 </form>
